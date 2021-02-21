dcd1d60a005f0adab79dc968eeed59c936d6ca5f
TabithaAluel <alueltabitha@gmail.com>
2021-02-21 10:27:13 +0300

    first commit

---
+++ src/student.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var fullName = "Tabitha Aluel"
+    println(fullName)
+    var age = "23"
+    println(age)
+    var phoneNumber = "0798008326"
+    println(phoneNumber)
+    var weight = "60"
+    println(weight)
+    var citizen = "southSudanese"
+    println(citizen)
+}
